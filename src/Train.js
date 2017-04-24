31cc2e461fa88c366e910a58b4ebcccfb433bd3e
Rudolf Hersén <rudolf.hersen@schibsted.se>
2017-04-24 17:12:37 +0200

    Move delay.js to Train.js

--- src/Train.js
+++ src/Train.js
@@ -2,17 +2,23 @@ import React, {Component} from 'react';
 import difference_in_seconds from 'date-fns/difference_in_seconds'
 import map from 'lodash.map'
 
-import * as delay from './delay'
+import difference_in_minutes from 'date-fns/difference_in_minutes'
+
 import * as wgs from './wgs'
 
 export default class Train extends Component {
     render() {
         const a = this.props.train.actual
         return <div key={a.AdvertisedTrainIdent}
-                    style={{color: delay.color(a), textAlign: x(a.LocationSignature, this.props.stations)}}>
+                    style={{color: color(a), textAlign: x(a.LocationSignature, this.props.stations)}}>
             {formatLatestAnnouncement(this.props.train, this.props.stations)}
         </div>
     }
+}
+
+function color(a) {
+    const delay = minutes(a)
+    return delay < 1 ? '#0f0' : delay < 2 ? '#fff' : delay < 4 ? '#ff0' : delay < 8 ? '#f80' : '#f00'
 }
 
 function x(location, stations) {
@@ -37,7 +43,7 @@ function formatLatestAnnouncement(train, stations) {
 
     return <span>
         {id(a)}
-        mot {to(a)} {activity(a)} {location(a)} {delay.precision(a)} {a.TimeAtLocation.substring(11, 16) + next(train)}
+        mot {to(a)} {activity(a)} {location(a)} {precision(a)} {a.TimeAtLocation.substring(11, 16) + next(train)}
         </span>
 
     function to() {
@@ -75,4 +81,18 @@ function activity(a) {
     return a.ActivityType === 'Ankomst' ?
         'ank' :
         'avg'
+}
+
+function precision(a) {
+    const delay = minutes(a)
+
+    return delay === 1 ? '' :
+        delay > 0 ? `${delay} min
+                           sent` : delay < -1 ? 'i god tid' :
+            ''
+
+}
+
+function minutes(a) {
+    return difference_in_minutes(a.TimeAtLocation, a.AdvertisedTimeAtLocation)
 }
